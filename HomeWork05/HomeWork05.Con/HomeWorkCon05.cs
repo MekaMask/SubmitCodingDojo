98e4e1ba84098e90ad677eca835cc2ff638a2063
maskmeka <agapevb35@gmail.com>
2018-11-21 15:34:33 +0700

    homework5

--- HomeWork05/HomeWork05.Con/HomeWorkCon05.cs
+++ HomeWork05/HomeWork05.Con/HomeWorkCon05.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using Homework05.Lib;
 
-namespace HomeWork05.Con
+namespace Homework05.Con
 {
-    class Program
+    class HomeWorkCon05
     {
         static void Main(string[] args)
         {
-            // var svc = new ();
+            var svc = new HomeWorkLib05();
 
             System.Console.WriteLine(@"[ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ]
  1   2   3   4   5   6   7   8   9   A");
@@ -21,14 +21,14 @@ namespace HomeWork05.Con
                 var isCorrectNumber = int.TryParse(ledSwiche, out var ledNoText) && ledNoText >= 1 && ledNoText <= 9;
                 var isA = ledSwiche.ToUpper() == "A";
 
-                // if (isCorrectNumber || isA)
-                // {
-                //     Console.WriteLine(svc.DisplayLEDOnScreen(ledSwiche));
-                // }
-                // else
-                // {
-                //     Console.WriteLine("Please enter 1-9 or a/A ");
-                // }
+                if (isCorrectNumber || isA)
+                {
+                    Console.WriteLine(svc.DisplayLEDOnScreen(ledSwiche));
+                }
+                else
+                {
+                    Console.WriteLine("Please enter 1-9 or a/A ");
+                }
             }
         }
     }
