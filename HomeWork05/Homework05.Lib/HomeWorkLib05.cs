7dbd9eb3a562cbd429ebdcc6ec4cf21761558a7e
mekamask <agapevb35@gmail.com>
2018-11-21 08:35:56 +0700

    new File

---
+++ HomeWork05/Homework05.Lib/HomeWorkLib05.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Homework05.Lib
+{
+    public class HomeWorkLib05 : IHomework05
+    {
+        
+        public string DisplayLEDOnScreen(string ledNo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HomeWork05/Homework05.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using Homework05.Lib;
+
+namespace Homework05.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var sut = new HomeWorkLib05();
+        }
+    }
+}
